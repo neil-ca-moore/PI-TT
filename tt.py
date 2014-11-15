c0ff51e8f410d2305533aaf6c8354b96213ff81c
Neil Moore <neil@cfconline.co.uk>
2014-11-15 15:39:16 +0000

    Improve the interface so each function is once only but takes a player as a parameter

--- tt.py
+++ tt.py
@@ -17,33 +17,39 @@ class Game:
 		self.playerB = PlayerState()
 		self.next_player_is_A = player_A_goes_first
 
-	def A_to_serve(self):
-		return self.next_player_is_A
+	def get_A(self):
+		return self.playerA
 
-	def B_to_serve(self):
-		return not self.A_to_serve()
+	def get_B(self):
+		return self.playerB
+
+	def other_player(self, player):
+		if player == self.playerA:
+			return self.playerB
+		else:
+			return self.playerA
+
+	def to_serve(self, player):
+		if player == self.get_A():
+			return self.next_player_is_A
+		else:
+			return not self.next_player_is_A
 
 	@classmethod
 	def have_won(cls, candidate_winner_score, candidate_loser_score):		
 		return candidate_winner_score >= cls.POINTS_TO_WIN and candidate_winner_score >= candidate_loser_score + 2
 
-	def A_has_won(self):
-		return Game.have_won(self.playerA.get_score(), self.playerB.get_score())
-
-	def B_has_won(self):
-		return Game.have_won(self.playerB.get_score(), self.playerA.get_score())
-
-	def A_scores(self):
-		self.player_scored(self.playerA)
-
-	def B_scores(self):
-		self.player_scored(self.playerB)
-
-	def total_points_scored(self):
-		return self.playerA.get_score() + self.playerB.get_score()
+	def has_won(self, player):
+		return Game.have_won(player.get_score(), self.other_player(player).get_score())
 
 	def player_scored(self, player):
 		player.scored_point()
 		total_points = self.total_points_scored()
 		if total_points >= 2 * self.POINTS_TO_WIN - 1 or total_points % self.SERVES_EACH == 0:
 			self.next_player_is_A = not self.next_player_is_A
+
+	def scores(self, player):
+		self.player_scored(player)
+
+	def total_points_scored(self):
+		return self.playerA.get_score() + self.playerB.get_score()
