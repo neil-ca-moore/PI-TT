3804f2c026ae513a26cc6930bd9c4451d9ca7df6
Neil Moore <neil@cfconline.co.uk>
2014-11-15 16:38:57 +0000

    Add doubles mode and also safety checking for scores() function

--- tt.py
+++ tt.py
@@ -9,13 +9,24 @@ class PlayerState:
 		self.score += 1
 
 class Game:
-	SERVES_EACH = 2
-	POINTS_TO_WIN = 11
-
-	def __init__(self, player_A_goes_first):
+	def __init__(self, player_A_goes_first, is_doubles = False):
 		self.playerA = PlayerState()
 		self.playerB = PlayerState()
 		self.next_player_is_A = player_A_goes_first
+		if is_doubles:
+			self.serves_each = 5
+			self.points_to_win = 21
+		else:
+			self.serves_each = 2
+			self.points_to_win = 11
+
+	@classmethod
+	def make_singles_game(cls, player_A_goes_first):
+		return cls(player_A_goes_first, False)
+
+	@classmethod
+	def make_doubles_game(cls, player_A_goes_first):
+		return cls(player_A_goes_first, True)
 
 	def get_A(self):
 		return self.playerA
@@ -35,21 +46,25 @@ class Game:
 		else:
 			return not self.next_player_is_A
 
-	@classmethod
-	def have_won(cls, candidate_winner_score, candidate_loser_score):		
-		return candidate_winner_score >= cls.POINTS_TO_WIN and candidate_winner_score >= candidate_loser_score + 2
+	def winning_score_check(self, candidate_winner_score, candidate_loser_score):		
+		return candidate_winner_score >= self.points_to_win and candidate_winner_score >= candidate_loser_score + 2
 
 	def has_won(self, player):
-		return Game.have_won(player.get_score(), self.other_player(player).get_score())
+		return self.winning_score_check(player.get_score(), self.other_player(player).get_score())
 
 	def player_scored(self, player):
 		player.scored_point()
 		total_points = self.total_points_scored()
-		if total_points >= 2 * self.POINTS_TO_WIN - 1 or total_points % self.SERVES_EACH == 0:
+		if total_points >= 2 * self.points_to_win - 1 or total_points % self.serves_each == 0:
 			self.next_player_is_A = not self.next_player_is_A
 
 	def scores(self, player):
-		self.player_scored(player)
+		if self.has_won(player):
+			raise "Player has already won!"
+		elif self.has_won(self.other_player(player)):
+			raise "Player has already lost!"
+		else:
+			self.player_scored(player)
 
 	def total_points_scored(self):
 		return self.playerA.get_score() + self.playerB.get_score()
